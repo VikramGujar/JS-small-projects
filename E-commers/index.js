2580d5bab89fd49eddaafd38381155315714ed19
VikramGujar <vikramgujar300@gmail.com>
2024-08-23 20:42:52 +0530

    final project from Prasad sir E-commers

---
+++ E-commers/index.js
@@ -0,0 +1,48 @@
+const mainDiv = document.getElementById('productList');
+
+const fetchData = async () =>
+    {
+        try
+        {
+            const res = await fetch('https://dummyjson.com/products');
+        const data = await res.json();
+        displayData(data);
+        }
+        
+        catch(err)
+        {
+            console.log(err);
+
+
+        }
+
+
+
+    }
+    fetchData();
+
+    const displayData = (data) =>
+        {
+            data.products.forEach((p) => 
+            {
+                const proDiv= document.createElement('div');
+                proDiv.classList.add('product');
+
+                const proImage = document.createElement('img');
+                proImage.src=p.thumbnail;
+                proImage.alt=p.title;
+
+                const proTitle = document.createElement('h2');
+                proTitle.textContent=p.title;
+
+                const proPrice = document.createElement('p');
+                proPrice.textContent="Price: $"+p.price;
+
+                const addCartButton = document.createElement('button');
+                addCartButton.textContent="ADD CART";
+
+                proDiv.append(proImage,proPrice,proTitle,addCartButton);
+                mainDiv.appendChild(proDiv)
+            });
+        }
+
