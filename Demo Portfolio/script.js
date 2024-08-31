9236caf30e1164b0ee7ba97ac93774db9aa2ee14
VikramGujar <vikramgujar300@gmail.com>
2024-08-31 20:38:16 +0530

    demo portfolio modified

--- Demo Portfolio/script.js
+++ Demo Portfolio/script.js
@@ -4,3 +4,45 @@
     typeSpeed: 80,
     loop: true
   });
+
+  anime({
+    targets: '.social-icons .ic',
+    translateX: {
+      value: 260,
+      duration: 800
+    },
+    rotate: {
+      value: 360,
+      duration: 1800,
+      easing: 'easeInOutSine'
+    },
+    scale: {
+      value: 1,
+      duration: 1600,
+      delay: 800,
+      easing: 'easeInOutQuart'
+    },
+    delay: 250 // All properties except 'scale' inherit 250ms delay
+  });
+
+  anime({
+    targets: '.dir-normal',
+    translateX: 250,
+    easing: 'easeInOutSine'
+  });
+  
+  anime({
+    targets: '.dir-reverse',
+    translateX: 250,
+    direction: 'reverse',
+    easing: 'easeInOutSine'
+  });
+  
+  anime({
+    targets: '.dir-alternate',
+    translateX: 250,
+    direction: 'alternate',
+    easing: 'easeInOutSine'
+  });
+
+ 
